311004feeca8a0aaf0e994fd052cbd2d462caad9
EduardoMoraes <eduzinho2118@gmail.com>
2025-01-25 08:02:15 -0300

    mudei a linguagem

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,22 @@ import './App.css'
 import Button from './components/Button'
 
 function App() {
+  const name = 'eduardo'
+  const newname = name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
   return (
     <>
-      <Button/>
+      <div className="App">
+        <h2>Alterando o JSX</h2>
+        <p>Olá, {newname}</p>
+        <p>Soma: {sum(3, 2)}</p>
+
+        <Button />
+      </div>
     </>
   )
 }
