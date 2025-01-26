079b9d1df9500e325e2e480ff85b1b69bb69c963
EduardoMoraes <eduzinho2118@gmail.com>
2025-01-25 21:11:25 -0300

    mudei appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,12 @@ function App() {
     return a + b
   }
 
-  const url = 'https://i1.wp.com/www.multarte.com.br/wp-content/uploads/2019/03/pikachu-150x150-png.png?fit=696%2C749&ssl=1'
-
   return (
     <>
       <div className="App">
         <h2>Alterando o JSX</h2>
         <p>Olá, {newname}</p>
         <p>Soma: {sum(3, 2)}</p>
-
-        <img src={url} alt="minha imagem" />
         
         <HelloWorld/>
       </div>
